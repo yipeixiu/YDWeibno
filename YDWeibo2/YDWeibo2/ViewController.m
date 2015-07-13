60dc721e50bac92ac956175686bc208304163cab
yipeixiu <yipeixiu.@ypx.cn>
2015-07-13 11:32:21 +0800

    添加了代码1
     添加了代码 1
     添加了代码 2

--- YDWeibo2/YDWeibo2/ViewController.m
+++ YDWeibo2/YDWeibo2/ViewController.m
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // 添加了代码 1
+    // 添加了代码 2
 }
 
 - (void)didReceiveMemoryWarning {
